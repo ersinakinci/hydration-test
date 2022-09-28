e32a5f080d1ed3a89fa404da31d7afc75d5d323a
Ersin Akinci <me@ersinakinci.com>
2022-09-28 11:34:33 -0700

    add edit pages

---
+++ pages/edit-project-template.tsx
@@ -0,0 +1,8 @@
+import { BuilderComponent, builder } from '@builder.io/react';
+
+// Replace with your Public API Key.
+builder.init('080513e0d5ae4bf78ed0b86e8ac876c6');
+
+export default function Page() {
+  return <BuilderComponent model="project-template" />;
+}
